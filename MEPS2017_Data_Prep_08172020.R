7ce27c1403943720818e267c7dbc1c608aa3e7ca
ShreevidyaPeriyasamy <shreevidya.periyasamy@gmail.com>
2020-08-17 20:17:40 -0400

    Data preparation - loading the consolidated meps2017_full data set

---
+++ MEPS2017_Data_Prep_08172020.R
@@ -0,0 +1,39 @@
+#---------#---------#---------#---------#---------#---------#
+
+# Project: AHRQ - MEPS 2017 Consolidated Data File Analysis
+# Creator: Shreevidya Periyasamy 
+# Contact: shreevidya.periyasamy@tufts.edu
+# Creation Date: 08.17.2020
+
+#---------#---------#---------#---------#---------#---------#
+
+
+# Question: Are there differences in access to care based 
+#           on insurance coverage type?
+
+
+#---------#---------#---------#---------#---------#---------#
+
+# Intializing required libraries
+library(car)
+library(foreign)
+library(leaps)
+library(MASS)
+library(psych)
+library(stats)
+library(table1)
+library(tidyverse)
+
+# Loading dataset into the environment
+# MEPS2017_full <- read.csv("h201.csv
+
+
+# Loading dataset from the AHRQ - MEPS website
+url <- "https://meps.ahrq.gov/mepsweb/data_files/pufs/h201ssp.zip"
+
+download.file(url, temp <- tempfile())
+
+meps2017_full <- unzip(temp, exdir = tempdir())
+
+dn2017 <- read.xport(meps2017_full)
+meps2017_full <- read.xport(meps2017_full)
